f8d9a80ac0a1136ef5db5bc09137c5584d16d448
Hannah <gsroquette@gmail.com>
2025-07-04 19:25:43 +0700

    Update payments.js

--- js/payments.js
+++ js/payments.js
@@ -15,7 +15,7 @@ import {
 const profileCache = new Map();
 
 /* ------------------------------------------------------------------
- * 1. LISTAR PAGAMENTOS dos alunos
+ * 1. LISTAR PAGAMENTOS
  * ----------------------------------------------------------------*/
 export async function listPayments(stuId) {
   const UL = $('payments-list');
